4d5063900bd88b013284c9b6365110455bdf1270
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-03-27 12:09:24 +0300

    feat: user service added

---
+++ src/main/java/me/jazzy/opos/service/UserService.java
@@ -0,0 +1,34 @@
+package me.jazzy.opos.service;
+
+import lombok.AllArgsConstructor;
+import me.jazzy.opos.model.User;
+import me.jazzy.opos.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User with " + email + " not found"));
+    }
+
+    public void saveUser(User user) {
+
+        boolean isEmailAlreadyTaken = userRepository.findByEmail(user.getEmail())
+                .isPresent();
+
+        if(isEmailAlreadyTaken)
+            throw new UsernameNotFoundException("Email already taken by another user");
+
+        userRepository.save(user);
+    }
+}
